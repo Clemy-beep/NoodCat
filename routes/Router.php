bed4241b4fcf0a7d6750390033a4cc1b786cfe71
Clemy-beep <clementine.digny@gmail.com>
2022-01-27 09:38:52 +0100

    router + model + entitymanagerhelper

---
+++ routes/Router.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Router;
+
+class Router
+{
+
+    private $url;
+    private $routes = [];
+    private $namedRoutes = [];
+
+    public function __construct($url)
+    {
+        $this->url = $url;
+    }
+
+    public function get($path, $callable, $name = null)
+    {
+        return $this->add($path, $callable, $name, 'GET');
+    }
+
+    public function post($path, $callable, $name = null)
+    {
+        return $this->add($path, $callable, $name, 'POST');
+    }
+
+    public function put($path, $callable, $name = null)
+    {
+        return $this->add($path, $callable, $name, 'PUT');
+    }
+
+    private function add($path, $callable, $name, $method)
+    {
+        $route = new Route($path, $callable);
+        $this->routes[$method][] = $route;
+        if (is_string($callable) && $name === null) {
+            $name = $callable;
+        }
+        if ($name) {
+            $this->namedRoutes[$name] = $route;
+        }
+        return $route;
+    }
+
+    public function run()
+    {
+        if (!isset($this->routes[$_SERVER['REQUEST_METHOD']])) {
+            throw new RouterException('REQUEST_METHOD does not exist');
+        }
+        foreach ($this->routes[$_SERVER['REQUEST_METHOD']] as $route) {
+            if ($route->matche($this->url)) {
+                return $route->call();
+            }
+        }
+        // echo "<pre>" . print_r($this->routes, true) . "</pre>";
+        http_response_code(404);
+        echo "Page not found.";
+    }
+
+    public function url($name, $params = [])
+    {
+        if (!isset($this->namedRoutes[$name])) {
+            throw new RouterException('No route matches this name');
+        }
+        return $this->namedRoutes[$name]->getUrl($params);
+    }
+}
