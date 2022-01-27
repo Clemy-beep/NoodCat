6a164a062aec498846d33ff8379f20e96d05c354
major55 <melicourtjul@outlook.fr>
2022-01-27 13:51:57 +0100

    push du tous

---
+++ src/Entity/CEO.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+class CEO extends Personne{
+
+    public function __construct(int $id, string $nom, string $prenom, string $email, string $pwd, int $registration)
+    {
+        parent::__construct($id, $nom,$prenom,$email,$pwd);
+    }
+}
