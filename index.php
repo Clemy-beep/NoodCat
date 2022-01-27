31d95bee9a74043ac4b10aeed8533707a4301e1d
Clemy-beep <clementine.digny@gmail.com>
2022-01-27 16:59:27 +0100

    initiated router

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php echo "coucou" ?>
+<?php
+require __DIR__ . '/vendor/autoload.php';
+require "bootstrap.php";
+
+use Router\Router;
+use App\Controller\AppController;
+
+$router = new Router($_GET['url']);
+$router->get("/", function(){
+    AppController::index();
+});
+
+$router->run();
